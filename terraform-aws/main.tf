9878284fe05fd0c581f904429fa774ae71bf9d1c
Shrikant <shrikant123@ubuntucloud.lan>
2026-03-26 11:13:00 +0530

    .

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "my_ec2" {
 ami            ="ami-017535a27f2ac0ce3"
 instance_type  ="t3.micro"
 vpc_security_group_ids = [aws_security_group.web_sg.id]
-key_name    = "ec2-key"   
+   
  
 tags = {
  Name = "terraform-ec2-shrik"
