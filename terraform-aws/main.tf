98129d9e6676910be963d2b72de34ef54b69914b
Shrikant <shrikant123@ubuntucloud.lan>
2026-03-26 11:01:09 +0530

    .

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -9,16 +9,47 @@ resource "aws_s3_bucket" "my_bucket" {
 bucket = "shrik-s3-bucket-96741"
 
 }
-resource "aws_instance" "my_ec2" {
-ami            ="ami-017535a27f2ac0ce3"
-instance_type  ="t3.micro"
-tags = {
- Name = "terraform-ec2-shrik"
-}
-}          
 
 # This tells Terraform to save the IP so Jenkins can read it
 output "ec2_public_ip" {
   value       = aws_instance.my_ec2.public_ip
   description = "The public IP of the web server"
 }
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and app access"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "my_ec2" {
+ami            ="ami-017535a27f2ac0ce3"
+instance_type  ="t3.micro"
+vpc_security_group_ids = [aws_security_group.web_sg.id]
+key_name    = "ec2-key"   
+ 
+tags = {
+ Name = "terraform-ec2-shrik"
+}
+}    
